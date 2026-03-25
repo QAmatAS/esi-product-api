599897133a43c59c443b96165dc566b22479358b
Narendra Atha Abhinaya <71386909+QAmatAS@users.noreply.github.com>
2026-03-26 01:24:44 +0700

    Bab 7

--- src/main/java/com/example/product_api/ProductApiApplication.java
+++ src/main/java/com/example/product_api/ProductApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductApiApplication {
+	public class ProductApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApiApplication.class, args);
--- src/main/java/com/example/product_api/controller/ProductController.java
+++ src/main/java/com/example/product_api/controller/ProductController.java
@@ -3,11 +3,16 @@ package com.example.product_api.controller;
 import com.example.product_api.dto.ProductRequest;
 import com.example.product_api.dto.ProductResponse;
 import com.example.product_api.service.ProductService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/products")
@@ -23,7 +28,19 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ProductResponse createProduct(@RequestBody ProductRequest request) {
+    public ProductResponse createProduct(@Valid @RequestBody ProductRequest request) {
         return productService.createProduct(request);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return errors;
     }
 }
